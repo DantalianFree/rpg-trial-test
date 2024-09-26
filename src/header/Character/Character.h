b5951f469620d605fb20c08b6ab5b7cc3a0448bb
DantalianFree <mdrojo.chmsu@gmail.com>
2024-09-26 18:26:31 +0800

    Create Character.h

---
+++ src/header/Character/Character.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string classType;
+    std::string playerName;
+    int health, maxHealth, damage, defense;
+
+public:
+    //Constructor/Destructor
+    Character( std::string classType, std::string playerName, int health, int maxHealth, int damage, int defense );
+    virtual ~Character();
+
+    //GETTERS
+    std::string getClassType() const;
+    std::string getName() const;
+    int getHealth() const;
+    int getMaxHealth() const;
+    int getDamage() const;
+    int getDefense() const;
+
+    //SETTERS
+    void setClassType( std::string newClassType );
+    void setName( std::string newName );
+    void setHealth( int newHealth );
+    void setMaxHealth( int newMaxHealth );
+    void setDamage( int newDamage );
+    void setDefense( int newDefense );
+
+    //Actions
+    void takeDamage( int damage );
+    int attack();
+    void heal( int amount );
+
+    //Display
+    void displayCharacter() const;
+
+};
