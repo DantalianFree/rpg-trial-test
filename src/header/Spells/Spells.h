515876207a38739963cb8023abe0b8c0fc21b36f
DantalianFree <mdrojo.chmsu@gmail.com>
2024-09-28 00:37:23 +0800

    Create Spells.h

---
+++ src/header/Spells/Spells.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Spell {
+private:
+    std::string spellName;
+    int damage, manaCost;
+
+public:
+    Spell( std::string spellName, int damage, int manaCost );
+    ~Spell();
+
+    std::string getSpellName() const;
+    int getDamage() const;
+    int getManaCost() const;
+
+    void setSpellName( std::string newSpellName );
+    void setDamage( int newDamage );
+    void setManaCost( int newManaCost );
+
+    void displaySpell() const;
+
+};
+
+class Spells {
+private:
+    std::vector<Spell> spellBook;
+
+public:
+    void addSpell()
+
+}
