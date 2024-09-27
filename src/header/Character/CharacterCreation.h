f5c9112ec9317bfaf5e2e708b8ccf588a6b8cead
DantalianFree <mdrojo.chmsu@gmail.com>
2024-09-27 21:53:47 +0800

    Update CharacterCreation.h

--- src/header/Character/CharacterCreation.h
+++ src/header/Character/CharacterCreation.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "Character.h"
+#include "Warrior.h"
 #include <iostream>
 #include <string>
 
 class Create {
 public:
-    static Character characterCreation( int cType ) {
+    static Character* characterCreation( int cType ) {
         switch( cType ) {
             case 1:
-                
+                return new Warrior();
         }
     }
 
-}
+};
