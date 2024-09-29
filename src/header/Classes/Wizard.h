b034823ae3c581b2fd625043ada3bd4c2d9024c8
DantalianFree <mdrojo.chmsu@gmail.com>
2024-09-29 20:58:28 +0800

    Create Wizard.h

---
+++ src/header/Classes/Wizard.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Character.h"
+#include "Spells.h"
+#include <iostream>
+
+class Wizard : public Character {
+private:
+    int mana, maxMana;
+
+public:
+    //Constructor for Wizard
+    Wizard();
+
+
+
+
+};
