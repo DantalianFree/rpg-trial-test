9fe69a541b2d13e59e313204c3574b7130cc33e5
DantalianFree <mdrojo.chmsu@gmail.com>
2024-10-04 16:53:38 +0800

    Update CombatSystem.h

--- src/header/Combat/CombatSystem.h
+++ src/header/Combat/CombatSystem.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Character.h"
+#include "Enemy.h"
 
-class CombatSystem {
-private:
-    
-
+class Combat {
 public:
-
+    void startCombat( Character* player, Enemy& enemy);
+    void combatStatus() const;
 
 };
