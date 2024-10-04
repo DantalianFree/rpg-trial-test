52bb0789f2d43243c4e39af2129360e5f3b15984
DantalianFree <mdrojo.chmsu@gmail.com>
2024-10-04 16:53:40 +0800

    Create Enemies.h

---
+++ src/header/Enemy/Enemies.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Enemy.h"
+
+Enemy Direwolf(
+    "Direwolf", //Enemy Type
+    8,           //Damage
+    5,           //Defense
+    10,          //Health
+    10           //MaxHealth
+);
+
+Enemy FeralDog(
+    "Feral Dog", //Enemy Type
+    4,           //Damage
+    2,           //Defense
+    5,          //Health
+    5           //MaxHealth
+);
+
+Enemy Goblin(
+    "Goblin", //Enemy Type
+    5,           //Damage
+    3,           //Defense
+    7,          //Health
+    7           //MaxHealth
+);
+
